e520d4e6ea3192db29415bbb3932f13cfe88e9be
John Desrosiers <johndrunner@yahoo.com>
2017-08-25 20:53:21 -0700

    add initial buttons and set even listeners

--- assets/javascript/gifTastic.js
+++ assets/javascript/gifTastic.js
@@ -1,22 +1,55 @@
 var topics = ["monty python", "star wars", "fails", "cats", "aviation"];
 
-// render topic buttons for each topic in topics
-// listen for click on #buttonBox
-// listen for click on #giffery
-// listen for click on #btnAddTopic
 
-// when a topic button is clicked
-// change the status bar to loading gifs
-// get the topic of the button
-// request 10 gifs from giphy api for topic
+$(document).ready( function() {
 
-// when a response is received from giphy
-// clear #giffery
-// apend images in #giffery for each gif
+	// render topic buttons for each topic in topics
+	// adding them to #buttonBox
+	topics.forEach(function(topic) {
+		$("<button>")
+			.addClass("u-full-width")
+			.text(topic)
+			.appendTo("#buttonBox");
+	});
 
-// when #giffery is clicked
-// get the clicked image
-// if the image is static
-	// animate the image
-// if the image is animated
-	// make the img static
+	// listen for click on #buttonBox
+	$("#buttonBox").on("click", handleTopicSelection);
+
+	// listen for click on #giffery
+	$("#giffery").on("click", toggleImage);
+
+	// listen for click on #btnAddTopic
+	$("#btnAddTopic").on("click", addTopic)
+
+	function addTopic(event) {
+	// Adds new topic button
+		event.preventDefault();
+	}
+
+	function handleTopicSelection() {
+	// Sends request to giphy api for gifs
+
+	// change the status bar to loading gifs
+	// get the topic of the button
+	// request 10 gifs from giphy api for topic
+	}
+
+	function renderGifs() {
+	// when a response is received from giphy
+	// clear #giffery
+	// apend images in #giffery for each gif
+	}
+
+	function toggleImage(img) {
+	// Toggles the state of img--animated/static
+
+	// get the clicked image
+	// if the image is static
+		// animate the image
+	// if the image is animated
+		// make the img static
+	}
+	
+
+});
+
