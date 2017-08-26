c8cd7ded5a209ffda7151fffc8ab5e9dd9383baa
John Desrosiers <johndrunner@yahoo.com>
2017-08-25 20:27:53 -0700

    better, more simple pseudo code

---
+++ assets/javascript/gifTastic.js
@@ -0,0 +1,22 @@
+var topics = ["monty python", "star wars", "fails", "cats", "aviation"];
+
+// render topic buttons for each topic in topics
+// listen for click on #buttonBox
+// listen for click on #giffery
+// listen for click on #btnAddTopic
+
+// when a topic button is clicked
+// change the status bar to loading gifs
+// get the topic of the button
+// request 10 gifs from giphy api for topic
+
+// when a response is received from giphy
+// clear #giffery
+// apend images in #giffery for each gif
+
+// when #giffery is clicked
+// get the clicked image
+// if the image is static
+	// animate the image
+// if the image is animated
+	// make the img static
