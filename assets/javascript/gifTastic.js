72d11108c69d88d37a876af771645749613dacd8
John Desrosiers <johndrunner@yahoo.com>
2017-08-25 22:27:38 -0700

    added renderGifs function and getFigure function

--- assets/javascript/gifTastic.js
+++ assets/javascript/gifTastic.js
@@ -3,6 +3,12 @@ var topics = ["monty python", "star wars", "fails", "cats", "aviation"];
 
 $(document).ready( function() {
 
+	// image state enumeration
+	var imgState = {
+		still: 0,
+		animated: 1
+	};
+
 	// render topic buttons for each topic in topics
 	// adding them to #buttonBox
 	topics.forEach(function(topic) {
@@ -22,9 +28,31 @@ $(document).ready( function() {
 	// listen for click on #btnAddTopic
 	$("#btnAddTopic").on("click", addTopic)
 
+// TODO
 	function addTopic(event) {
 	// Adds new topic button
 		event.preventDefault();
+	}
+
+	function getFigure(images, rating) {
+	// Returns jquery figure element containing an image with rating
+	// Parameters: images object and rating string
+
+		// return a new html figure element
+		return $("<figure>")
+
+			// append new image
+			.append(
+				$("<img>").attr( {
+					'src': images.fixed_height_still.url,
+					'data-state': imgState.still,
+					'data-static': images.fixed_height_still.url,
+					'data-animate': images.fixed_height.url
+					} ) )
+
+			// append rating to figure
+			.append($("<figcaption>")
+				.text("Rating: " + rating) );
 	}
 
 	function handleTopicSelection(event) {
@@ -36,30 +64,41 @@ $(document).ready( function() {
 		queryURL += $.param({
 			api_key: "f3971dc19c6240feab39b26de85716d1",
 			q: $(event.target).attr("data-topic"),
-			limit: 10
+			limit: 10,
+			rating: "pg-13"
 		});
 
 		// change the status bar to loading gifs
 		Window.status = "Loading gifs...";	
-
-		console.log(queryURL);
 
 		//send request
 		$.ajax({
 			url: queryURL,
 			method: "GET"
 		}).done(function(response) {
-			console.log(data);
-			renderGifs(response.data);
+			renderGifs(response);
 		});
 	}
 
-	function renderGifs(data) {
-	// when a response is received from giphy
-	// clear #giffery
-	// apend images in #giffery for each gif
+	function renderGifs(giphyData) {
+	// Renders images in data
+
+		var data = giphyData.data;
+		
+		// clear #giffery
+		$("#giffery").empty()
+
+		// apend images in #giffery for each gif with image in a
+		// static (as opposed to animated) state
+		for ( var i = 0; i < data.length; i++ ) {
+
+			// append figure to the giffery
+			var gifRating = giphyData.data[i].rating;
+			getFigure(data[i].images, gifRating).appendTo("#giffery");
+		}					
 	}
 
+// TODO
 	function toggleImage(img) {
 	// Toggles the state of img--animated/static
 
